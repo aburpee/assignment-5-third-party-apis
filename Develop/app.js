f37b9091f77c193be7745d656ce19d20e00ab4c6
bonobo <arburpee@gmail.com>
2022-02-24 17:14:34 -0800

    created html and added selectors

--- Develop/app.js
+++ Develop/app.js
@@ -18,11 +18,11 @@ if (moment(renderClock).isAfter($("#timeColumn09").text())) {
     window.alert('true')
 }
 var time = $("#timeColumn09").text()
-// window.alert(typeof($("#timeColumn09")));
 
-// time = function() {
-$("#timeColumn09").html('09:00:00')
-    // moment('09:00:00').format('hh:mm:ss'))
-// }
 
-time()
+
+$('.save-btn').on('click', function() {
+    let userEntry = $(this).siblings('input').val()
+    let timeBlock = $(this).siblings('input').attr('id')
+    console.log(userEntry, timeBlock)
+})
