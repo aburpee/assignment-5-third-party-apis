13ab30cd2dde1198cbf347c3dcf1e49182425de9
bonobo <arburpee@gmail.com>
2022-02-24 18:45:14 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+
+![Home Page](./work-day-scheduler-readme.png)
+
+## Description
+This is an app that allows users to track their daily activities, meetings, appointments, etc. 
+
+It alerts users when their appointments have passed, when they're upcoming, and when they're imminent.
+
+## Technologies
+* javascript
+* css/bootstrap
+* jquery
+* moment.js
+
+## Contributors
+* Adam Burpee: [github](https://github.com/aburpee/assignment-5-third-party-apis)
+
+## Link
+https://aburpee.github.io/assignment-5-third-party-apis/
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//renders clock at top of screen
+
 let renderClock = function() {
     $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'))
 }
@@ -8,15 +10,20 @@ if (moment(renderClock).isAfter($("#timeColumn09").text())) {
 var time = $("#timeColumn09").text()
 
 
-
+// saves notes to local storage
 $('.save-btn').on('click', function() {
     let userEntry = $(this).siblings('input').val()
     let timeBlock = $(this).siblings('input').attr('id')
     console.log(userEntry, timeBlock)
     localStorage.setItem(timeBlock, userEntry)
 })
+
+// sets current hour using moment
 let currentHour = moment().hour()
 console.log(currentHour)
+
+// restablishes notes on refresh and adds colors to 
+// line items based on timeframe
 for (let i = 9; i <=17; i++) {
     $('#'+i).val(localStorage.getItem(i))
     if (currentHour > i) {
